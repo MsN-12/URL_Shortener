2dce1952f352e572b1e4b3d2209f87987eeb73f1
MsN <mohsen.s7s.ab@gmail.com>
2024-04-15 20:46:14 +0330

    Initial commit

---
+++ store/store_service.go
@@ -0,0 +1,49 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+const cacheDuration = time.Hour * 3
+
+var (
+	storageService = &StorageService{}
+	ctx            = context.Background()
+)
+
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Redis ping: ", pong)
+	storageService.redisClient = redisClient
+	return storageService
+}
+
+func SaveUrl(shortURL, originalURL, userID string) {
+	err := storageService.redisClient.Set(ctx, shortURL, originalURL, cacheDuration).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func RetriveUrl(shortURL, userID string) string {
+	result, err := storageService.redisClient.Get(ctx, shortURL).Result()
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
