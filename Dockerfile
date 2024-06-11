01b9b2d6e32a87de7f07e832a4bfab5d4f8da232
MsN <mohsen.s7s.ab@gmail.com>
2024-06-11 19:23:10 +0330

    add ui

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,7 @@ RUN apk --no-cache add ca-certificates
 WORKDIR /root/
 
 COPY --from=builder /app/main .
+COPY --from=builder /app/.env .
 
 EXPOSE 3000
 
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file: ", err)
 	}
 	redisAddr := os.Getenv("REDIS_ADDR")
 	redisPassword := os.Getenv("REDIS_PASSWORD")
